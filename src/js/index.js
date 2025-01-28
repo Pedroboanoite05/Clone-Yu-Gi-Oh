ea73d8ccfae93878326edeb56d767c53b1ea58e0
Pedroboanoite05 <pedrohboanoite@gmail.com>
2025-01-28 15:26:18 -0300

    Add files via upload

---
+++ src/js/index.js
@@ -0,0 +1,31 @@
+const btnavancar = document.getElementById("btn-avancar");
+const btnvoltar = document.getElementById("btn-voltar");
+const cartoes = document.querySelectorAll(".cartao");
+let cartaoAtual = 0;
+
+btnavancar.addEventListener("click", function(){
+    if (cartaoAtual === cartoes.length - 1) return;
+
+    esconderCartaoSelecionado();
+
+    cartaoAtual++;
+    mostrarCartao(cartaoAtual);
+});
+
+btnvoltar.addEventListener("click", function() {
+    if (cartaoAtual === 0) return;
+
+    esconderCartaoSelecionado();
+
+    cartaoAtual--;
+    mostrarCartao(cartaoAtual);
+});
+
+function mostrarCartao(cartaoAtual) {
+    cartoes[cartaoAtual].classList.add("selecionado");
+}
+
+function esconderCartaoSelecionado() {
+    const cartaoSelecionado = document.querySelector(".selecionado");
+    cartaoSelecionado.classList.remove("selecionado");
+}
